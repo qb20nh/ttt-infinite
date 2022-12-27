d430fc8598027e0ab9b7f539aef502cfd1cb8d3a
Taejong Yoo <yut951121@gmail.com>
2022-12-27 15:27:23 +0900

    feat: :art: yes

--- src/controller/board.ts
+++ src/controller/board.ts
@@ -1,12 +1,33 @@
+import { N } from 'ts-toolbelt'
+
+type DefaultMaxRange = N.Range<1, 3>
+type LevelRange = DefaultMaxRange[keyof DefaultMaxRange & number]
+
+export class BoardState {
+  move (): void {
+    console.log('boardState move')
+  }
+}
+
 export class Board {
   #width: number
   #height: number
-  constructor (width: number = 3, height: number = 3) {
+  #maxLevel: number
+
+  #state: BoardState
+
+  constructor ({ width = 3, height = 3, maxLevel }: { width: number, height: number, maxLevel: LevelRange }) {
     this.#width = width
     this.#height = height
+    this.#maxLevel = maxLevel
+    this.#state = new BoardState()
+  }
+
+  move (): void {
+    this.#state.move()
   }
 
   log (): void {
-    console.log(`${this.#width}x${this.#height}`)
+    console.log(`${this.#width}x${this.#height}@${this.#maxLevel}`)
   }
 }
